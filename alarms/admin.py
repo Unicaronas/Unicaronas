d638d4b5f49f7b582122c7de5d31712ce0d4f90d
Gustavo Maronato <gustavomaronato@gmail.com>
2018-11-04 03:33:38 -0200

    added alarms to django admin

--- alarms/admin.py
+++ alarms/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Alarm
+
+
+@admin.register(Alarm)
+class AlarmAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'created',
+        'user',
+        'origin',
+        'origin_point',
+        'origin_radius',
+        'destination',
+        'destination_point',
+        'destination_radius',
+        'price',
+        'auto_approve',
+        'datetime_lte',
+        'datetime_gte',
+        'min_seats',
+    )
+    list_filter = (
+        'created',
+        'user',
+        'auto_approve',
+        'datetime_lte',
+        'datetime_gte',
+    )
