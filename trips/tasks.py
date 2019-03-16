9839134d9080c58be8ec9761db0bfa891a5192e3
Gustavo Maronato <gustavomaronato@gmail.com>
2019-03-16 20:32:44 -0300

    render facebook posts using the correct timezones

--- trips/tasks.py
+++ trips/tasks.py
@@ -2,12 +2,14 @@ import facebook
 from django.conf import settings
 from django.contrib.humanize.templatetags.humanize import naturalday
 from celery import shared_task
+import pytz
 
 
 @shared_task
 def publish_new_trip_on_fb(trip_id):
     from .models import Trip
     if settings.FACEBOOK_PAGE_ACCESS_TOKEN:
+        sp = pytz.timezone('America/Sao_Paulo')
         graph = facebook.GraphAPI(
             access_token=settings.FACEBOOK_PAGE_ACCESS_TOKEN, version="3.1")
         trip = Trip.objects.get(id=trip_id)
@@ -15,13 +17,14 @@ def publish_new_trip_on_fb(trip_id):
         if trip.origin_address_components is None:
             return
 
+        datetime = trip.datetime.astimezone(sp)
         origin_city = trip.get_origin_adm_area_2()
         destination_city = trip.get_destination_adm_area_2()
 
         origin_state = trip.get_origin_adm_area_1(short=True)
         destination_state = trip.get_destination_adm_area_1(short=True)
 
-        time = f"{naturalday(trip.datetime, 'd/m')} às {trip.datetime.strftime('%H:%M')}".capitalize()
+        time = f"{naturalday(datetime, 'd/m')} às {datetime.strftime('%H:%M')}".capitalize()
 
         url = f"https://app.unicaronas.com/search/{trip.id}/"
 
